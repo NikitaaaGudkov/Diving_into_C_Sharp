d7695a5e48851a419250cbb655819a045e1eecb3
NikitaaaGudkov <nikita.gudkov.2015@mail.ru>
2023-12-20 23:01:57 +0300

    Homework 1 done

--- Homework_1/ConsoleApp1/ConsoleApp1/Program.cs
+++ Homework_1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double a = double.Parse(args[0]);
+string operation = args[1];
+double b = double.Parse(args[2].Replace(".", ","));
+switch (operation)
+{
+    case "+":
+    {
+        Console.WriteLine($"{a} + {b} = {a + b}");
+        break;
+    }
+    case "-":
+    {
+        Console.WriteLine($"{a} - {b} = {a - b}");
+        break;
+    }
+    case "*":
+    {
+        Console.WriteLine($"{a} * {b} = {a * b}");
+        break;
+    }
+    case "/":
+    {
+        if(b == 0)
+        {
+            Console.WriteLine("Ошибка! Деление на ноль!");
+        }
+        else
+        {
+            Console.WriteLine($"{a} / {b} = {a / b}");
+        }
+        break;
+    }
+    default:
+    {
+        Console.WriteLine($"Введена недопустимая операция: {operation}");
+        break;
+    }
+}
